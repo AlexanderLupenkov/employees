520eb69b579b79b1c406d6de625661796230dea4
Alexander Lupenkov <alexander.lupenkov@helmes.com>
2021-12-27 11:54:36 +0300

    change main class (delete mapperScan annotation)

--- src/main/java/com/altech/employees/EmployeesApplication.java
+++ src/main/java/com/altech/employees/EmployeesApplication.java
@@ -4,17 +4,13 @@ import com.altech.employees.domain.entity.Employee;
 import com.altech.employees.domain.entity.Organization;
 import com.altech.employees.domain.entity.Status;
 import org.apache.ibatis.type.MappedTypes;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.altech.employees.config.db.mapper")
 @MappedTypes({Employee.class, Organization.class, Status.class})
 public class EmployeesApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EmployeesApplication.class, args);
     }
-
 }
