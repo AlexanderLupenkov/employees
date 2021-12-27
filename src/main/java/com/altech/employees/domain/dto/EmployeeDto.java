0aebc319e845ef1f55d41727527a536aaf0f307b
Alexander Lupenkov <alexander.lupenkov@helmes.com>
2021-12-27 12:42:08 +0300

    add DTOs

--- src/main/java/com/altech/employees/domain/dto/EmployeeDto.java
+++ src/main/java/com/altech/employees/domain/dto/EmployeeDto.java
@@ -1,4 +1,13 @@
 package com.altech.employees.domain.dto;
 
+import lombok.Data;
+
+@Data
 public class EmployeeDto {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private OrganizationDto organization;
+    private StatusDto status;
 }
--- src/main/java/com/altech/employees/domain/dto/StatusDto.java
+++ src/main/java/com/altech/employees/domain/dto/StatusDto.java
@@ -1,4 +1,9 @@
 package com.altech.employees.domain.dto;
 
+import lombok.Data;
+
+@Data
 public class StatusDto {
+    private Integer id;
+    private String name;
 }
