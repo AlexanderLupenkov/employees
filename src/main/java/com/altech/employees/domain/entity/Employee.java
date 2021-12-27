309529806f2b4cbf1bc539a04a7edb7b7849f861
Alexander Lupenkov <alexander.lupenkov@helmes.com>
2021-12-27 12:42:26 +0300

    visual fix Employee.class

--- src/main/java/com/altech/employees/domain/entity/Employee.java
+++ src/main/java/com/altech/employees/domain/entity/Employee.java
@@ -3,6 +3,7 @@ package com.altech.employees.domain.entity;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -18,16 +19,16 @@ public class Employee {
     @NotEmpty
     private String lastName = "";
 
+    @Email
+    @NotEmpty
+    private String email = "";
+
     @NotNull
     @JsonIgnoreProperties({"employees"})
     private Organization organization;
 
     @NotNull
     private Status status;
-
-    @Email
-    @NotEmpty
-    private String email = "";
 
     @Override
     public String toString() {
